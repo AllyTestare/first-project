dc9cc4c342511d9e94630cb155fc6ab41b27a2db
AllyTestare <ally.testare@gmail.com>
2021-12-20 17:27:15 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,28 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+window.Vue = require('vue').default;
+
+import VueRouter from 'vue-router'
+// import routes from './routes'
+
+Vue.use(VueRouter)
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default }
+  ]
+
+const router = new VueRouter(routes)
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    router
+});
+
+
+
+
